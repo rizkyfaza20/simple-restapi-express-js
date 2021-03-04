ebe7e7498f8cec0c9634ef1de128c10444d57ed2
rizkyfaza20 <rizkyfaza61@gmail.com>
2021-03-04 11:35:37 +0700

    Change in any Files

---
+++ models/data-assets.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const data_asset = sequelize.define('data_asset',{
+    manufacturer:DataTypes.STRING,
+    modelBoard:DataTypes.STRING,
+    cpu:DataTypes.STRING,
+     
+
+  }, function(require, factory){
+    'use strict';
+    
+  }); {
+    data_asset = function(models){
+      // Define association here
+    };
+    return data_asset;
+  };
+  data - assets.init({
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    tags: DataTypes.STRING,
+    published: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'data-assets',
+  });
+  return data - assets;
+};
